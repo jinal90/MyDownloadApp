6345f7ffdf95e799a0203ec4394584e886580d1e
jinal90 <jinal010990@gmail.com>
2022-06-19 22:44:35 +0200

    Implementation: Notification on successfull Download

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -8,11 +9,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -34,19 +37,69 @@ class MainActivity : AppCompatActivity() {
 
         custom_button.setOnClickListener {
 
-           when(rg_downloadOptions.checkedRadioButtonId){
-               R.id.rb_Glide -> download("https://github.com/bumptech/glide/archive/refs/heads/master.zip")
-               R.id.rb_LoadApp -> download("https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/refs/heads/master.zip")
-               R.id.rb_Retrofit -> download("https://github.com/square/retrofit/archive/refs/heads/master.zip")
-               else -> Toast.makeText(this, "Please select a file to download", Toast.LENGTH_SHORT).show()
-           }
+            when (rg_downloadOptions.checkedRadioButtonId) {
+                R.id.rb_Glide -> download("https://github.com/bumptech/glide/archive/refs/heads/master.zip")
+                R.id.rb_LoadApp -> download("https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/refs/heads/master.zip")
+                R.id.rb_Retrofit -> download("https://github.com/square/retrofit/archive/refs/heads/master.zip")
+                else -> Toast.makeText(this, "Please select a file to download", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
+
+        createNotificationChannel()
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
             Log.d("test", "Download success")
+            Toast.makeText(context, "Download success!", Toast.LENGTH_SHORT).show()
+
+            val intent1 = Intent(context, DetailActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent =
+                PendingIntent.getActivity(context, 0, intent1, PendingIntent.FLAG_IMMUTABLE)
+
+            context?.let {
+                var builder =
+                    NotificationCompat.Builder(it, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_baseline_cloud_download_24)
+                        .setContentTitle(getString(R.string.app_name))
+                        .setStyle(
+                            NotificationCompat.BigTextStyle()
+                                .bigText("Downloaded File: ")
+                        )
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
+                        .addAction(
+                            R.drawable.ic_baseline_cloud_download_24, "Check Status",
+                            pendingIntent
+                        )
+
+
+                with(NotificationManagerCompat.from(context)) {
+                    notify(111, builder.build())
+                }
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "SomeChannelID"
+            val descriptionText = "Some description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
@@ -58,7 +111,7 @@ class MainActivity : AppCompatActivity() {
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+//                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
